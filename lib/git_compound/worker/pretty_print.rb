f520491b75663ea048ed46795cce83f620518bcc
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-06-16 11:56:27 +0200

    Refactor pp worker (have ancestors now)

--- lib/git_compound/worker/pretty_print.rb
+++ lib/git_compound/worker/pretty_print.rb
@@ -3,37 +3,18 @@ module GitCompound
     # Worker that prints dependency tree
     #
     class PrettyPrint < AbstractWorker
-      def initialize
-        @indent = 0
-      end
-
       def visit_component(component)
-        @indent = 0 if root_component?(component)
         print_component(component)
       end
 
-      def visit_manifest(manifest)
-        @root_manifest ||= manifest
-        @indent += 1
+      def visit_manifest(_manifest)
       end
 
       private
 
-      def root_component?(component)
-        root_components = @root_manifest.components.values
-        root_components.include?(component)
-      end
-
       def print_component(component)
-        print_line("`#{component.name}` component, #{component.source.version}")
-      end
-
-      def print_line(text)
-        puts indentation + text
-      end
-
-      def indentation
-        '  ' * @indent
+        print '  ' * component.ancestors.count
+        puts "`#{component.name}` component, #{component.source.version}"
       end
     end
   end
