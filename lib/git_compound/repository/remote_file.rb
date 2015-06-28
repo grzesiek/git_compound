438c120700d5c3f7d6fc7b43744d8b1526344dc0
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-06-28 12:51:30 +0200

    Choose remote file strategy depending on #reachable? (refactoring)

--- lib/git_compound/repository/remote_file.rb
+++ lib/git_compound/repository/remote_file.rb
@@ -12,20 +12,22 @@ module GitCompound
 
       def contents
         @strategies ||= strategies_available
-        @strategies.each do |strategy|
-          begin
-            return strategy.new(@source, @ref, @file).contents
-          rescue FileUnreachableError
-            next
-          end
+        @strategies.each do |remote_file_strategy|
+          remote_file = remote_file_strategy.new(@source, @ref, @file)
+          next unless remote_file.reachable?
+          return remote_file.contents
         end
         raise FileUnreachableError,
               "Couldn't reach file #{@file} after trying #{@strategies.count} stategies"
       end
 
       def strategies_available
-        [GitArchiveStrategy,
-         GithubStrategy]
+        # Strategies ordered by #reachable? method overhead
+        # More general strategies should be placed at lower positions,
+        # but this also depends on #reachable? overhead.
+        #
+        [GithubStrategy,
+         GitArchiveStrategy]
       end
     end
   end
