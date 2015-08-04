4e2296c7ac2ecd92a36fc353516d415a52d52638
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-08-04 12:54:59 +0200

    Improve pretty print message in `show` command

--- lib/git_compound/worker/pretty_print.rb
+++ lib/git_compound/worker/pretty_print.rb
@@ -4,14 +4,26 @@ module GitCompound
     #
     class PrettyPrint < Worker
       def visit_component(component)
-        print_component(component)
+        pretty_print(component, " `#{component.name}` component, #{component.version}")
+      end
+
+      def visit_manifest(manifest)
+        details = []
+        details << "Component: #{manifest.name}" unless manifest.name.empty?
+        details << "Maintainer: #{manifest.maintainer.join(', ')}" unless
+          manifest.maintainer.empty?
+        details << 'Dependencies:' unless manifest.components.empty?
+
+        pretty_print(manifest, *details)
       end
 
       private
 
-      def print_component(component)
-        Logger.inline '  ' * component.ancestors.count
-        Logger.info "`#{component.name}` component, #{component.version}"
+      def pretty_print(element, *messages)
+        messages.each do |message|
+          Logger.inline '    ' * element.ancestors.count
+          Logger.info message
+        end
       end
     end
   end
--- spec/workers/shared_examples/pretty_print.rb
+++ spec/workers/shared_examples/pretty_print.rb
@@ -1,11 +1,19 @@
 shared_examples 'pretty print worker' do
   it 'should pretty-print dependency tree' do
     expected_pattern =
-      '`component_1` component, version: ~>1.1.*?'        \
-      '  `leaf_component_1` component, version: 1.0.*?'   \
-      '  `leaf_component_2` component, version: 1.0.*?'   \
-      '`component_2` component, version: 1.1.*?'          \
-      '  `leaf_component_3` component, version: ~>1.0.*?'
+      'Component: base_component.*?'                             \
+      'Maintainer: Grzegorz Bizon <grzegorz.bizon@ntsn.pl>.*?'   \
+      'Dependencies:.*?'                                         \
+      ' `component_1` component, version: ~>1.1.*?'              \
+      '    Component: component_1.*?'                            \
+      '    Dependencies:.*?'                                     \
+      '     `leaf_component_1` component, version: 1.0.*?'       \
+      '     `leaf_component_2` component, version: 1.0.*?'       \
+      ' `component_2` component, version: 1.1.*?'                \
+      '    Component: component_2.*?'                            \
+      '    Maintainer: Grzegorz Bizon <grzesiek@ntsn.pl>.*?'     \
+      '    Dependencies:.*?'                                     \
+      '     `leaf_component_3` component, version: ~>1.0.*?'
 
     expect { subject.call }
       .to output(/^#{expected_pattern}\Z/m).to_stdout
